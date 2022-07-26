c6c22f139eeba61f95d5789ea4249e65b15327f6
Anh Le <haianhltr@yahoo.com.vn>
2022-07-27 05:14:23 +0700

    show connect wallet button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {useEffect} from "react"
+import { useEffect } from "react";
 
 const App = () => {
   const checkIfWalletIsConnected = async () => {
@@ -8,6 +8,11 @@ const App = () => {
       if (solana) {
         if (solana.isPhantom) {
           console.log("Phantom wallet found!");
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connect with public key: ",
+            response.publicKey.toString()
+          );
         } else {
           alert("Solana object not found! Get a Phantom wallet");
         }
@@ -16,16 +21,29 @@ const App = () => {
       console.error(error);
     }
   };
+
+  //function for connecting wallet
+  const connectWallet = async () => {
+
+  }
+
+  const renderNotConnectContainer = () => {
+    <button onClick={connectWallet}>Connect to wallet</button>
+  };
+
   useEffect(() => {
     //run when page is loaded
-    const onLoad = async() => {
-      await checkIfWalletIsConnected()
-    
-    }
-    window.addEventListener('load',onLoad)
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
     //return eventListener when window close
-    return () => window.removeEventListener("load", onLoad)
-  }, [])
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return (
+  <div className="App">{renderNotConnectContainer()}</div>
+  )
 };
 
 export default App;
