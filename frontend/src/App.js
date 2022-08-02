627165ee03e91522797d4db2f872725e8ced0256
Anh Le <haianhltr@yahoo.com.vn>
2022-08-02 16:36:31 +0700

    create campaign

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import idl from "./idl.json"
-import {Connection, PublicKey, clusterApiUrl} from "@solana/web3.js";
+import idl from "./idl.json";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 import {
   Program,
   AnchorProvider,
@@ -11,14 +11,14 @@ import {
 import { useEffect, useState } from "react";
 
 const programID = new PublicKey(idl.metadata.address);
-const network = clusterApiUrl("devnet")
+const network = clusterApiUrl("devnet");
 
 //ops control how we want to acknoledge when transaction is done
 // we can choose when to receive a confirmation when a transaction succedd.
 const opts = {
   preflightCommitment: "processed",
-}
-const {SystemProgram} = web3;
+};
+const { SystemProgram } = web3;
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
@@ -26,10 +26,14 @@ const App = () => {
   //provider is an authenticated connection to Solana
   // you can't even retrieve anything from Solana if you don't have a wallet
   const getProvider = () => {
-    const connection = new Connection(network,opts.preflightCommitment);
-    const provider = new AnchorProvider(connection, window.solana, opts.preflightCommitment)
-    return provider
-  }
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
+    return provider;
+  };
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -41,7 +45,7 @@ const App = () => {
             "Connect with public key: ",
             response.publicKey.toString()
           );
-          setWalletAddress(response.publicKey.toString())
+          setWalletAddress(response.publicKey.toString());
         } else {
           alert("Solana object not found! Get a Phantom wallet");
         }
@@ -53,18 +57,45 @@ const App = () => {
 
   //function for connecting wallet
   const connectWallet = async () => {
-    const {solana} = window;
-    if(solana)
-    {
-      const response = await solana.connect()
-      console.log('Connected with public key:', response.publicKey.toString())
-      setWalletAddress(response.publicKey.toString())
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with public key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
     }
-  }
+  };
+
+  const createCampaign = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const [campaign] = await PublicKey.findProgramAddress(
+        [
+          utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
+          provider.wallet.publicKey.toBuffer(),
+        ],
+        program.programId
+      );
+      await program.rpc.create("campaign name", "campaign description", {
+        accounts: {
+          campaign,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+      });
+      console.log("Created a new campaign w/ address:", campaign.toString());
+    } catch (error) {
+      console.error("Error creating campaign account: ", error);
+    }
+  };
 
   const renderNotConnectedContainer = () => (
-		<button onClick={connectWallet}>Connect to Wallet</button>
-	);
+    <button onClick={connectWallet}>Connect to Wallet</button>
+  );
+
+  const renderConnectedContainer = () => (
+    <button onClick={createCampaign}>Create a campaign..</button>
+  );
 
   useEffect(() => {
     //run when page is loaded
@@ -77,8 +108,8 @@ const App = () => {
   }, []);
 
   return (
-  <div className="App">{!walletAddress && renderNotConnectedContainer()}</div>
-  )
+    <div className="App">{!walletAddress && renderNotConnectedContainer()} {walletAddress && renderConnectedContainer()}</div>
+  );
 };
 
 export default App;
