8857f830237e4b04cdca24118bc00e187ddfc0a9
Anh Le <haianhltr@yahoo.com.vn>
2022-08-02 17:56:33 +0700

    button to display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -106,10 +106,22 @@ const App = () => {
 
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet}>Connect to Wallet</button>
+
   );
 
   const renderConnectedContainer = () => (
+    <>
     <button onClick={createCampaign}>Create a campaign..</button>
+    <button onClick = {getCampaigns}>Get a list of campaigns...</button>
+    <br/>
+    {campaigns.map(campaign => (<>
+    <p>Campaign ID: {campaign.pubkey.toString()}</p>
+    <p>Balance: {(campaign.amountDonated / web3.LAMPORTS_PER_SOL).toString()}</p>
+    <p>{campaign.name}</p>
+    <p>{campaign.description}</p>
+    <br/>
+     </>))}
+    </>
   );
 
   useEffect(() => {
